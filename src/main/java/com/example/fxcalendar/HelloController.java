1eede0d5d1b57d39ebe9f82e77a4b7eaa0a3e9f9
ayoub-djaghloul <ayoubdj19@gmail.com>
2024-02-20 15:30:05 +0100

    affichage d'un exemple de edt

--- src/main/java/com/example/fxcalendar/HelloController.java
+++ src/main/java/com/example/fxcalendar/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.fxcalendar;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URI;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    public void onPlanningButtonClick(javafx.event.ActionEvent actionEvent) {
+        try {
+            // Charge la nouvelle vue FXML pour l'emploi du temps
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("planning-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400); // Adaptez les dimensions selon votre besoin
+            Stage stage = new Stage();
+            stage.setTitle("Emploi du Temps");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module com.example.fxcalendar {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires org.mnode.ical4j.core;
+    requires jfxtras.icalendarfx;
 
     opens com.example.fxcalendar to javafx.fxml;
     exports com.example.fxcalendar;
